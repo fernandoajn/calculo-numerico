812c30989bee8f8e6434c278d38ffdbc0e08fb70
Fernando Augusto <fernandoajneto@gmail.com>
2018-08-11 21:36:08 -0300

    add equacao.py

---
+++ exercicios/equacao.py
@@ -0,0 +1,7 @@
+x = float(input("Entre com x: "))
+
+eq = raw_input("Entre com a equacao grau: <notacao em python> ")
+print("Sua equacao: {0} = 0".format(eq))
+
+fin = eval(eq)
+print("Resultado = {0}".format(fin))
