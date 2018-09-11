2e1745576668e5032ee32f87cede67ad89e7a11c
Fernando Augusto <fernandoajneto@gmail.com>
2018-09-10 22:31:40 -0300

    metodo da bisseccao

--- metodos/bisseccao.py
+++ metodos/bisseccao.py
@@ -1,14 +1,41 @@
-# Author: @ffaneto
+# Author: @fernandoajn
 import math
 
 print("Método da Bissecção")
 print("--------------------")
 
-eq = input("Digite a equação: <notação em python> ")
-f = eval(eq) # validar função
+def funcao(x):
+    eq = input("Digite a equação: <notação em python> ")
+    f = eval(eq) # validar função
+    return f
 
 a = input("Ponto A: ")
 b = input("Ponto B: ")
 E = input("Tolerancia: ")
 
-i = 1 # iteração
+i = 0 # iteração
+
+while True:
+    i += 1
+    fa = funcao(a)
+    fb = funcao(b)
+    x = (a*fb-b*fa)/(fb-fa)
+    fx = funcao(x)
+
+    if abs(fa) < e:
+        x = a
+        break
+    elif abs(fb) < e:
+        x = b
+        break
+    elif abs(fx) < e:
+        break
+    elif (fa*fx)<0
+        b = x
+    elif (fa*fx)>0:
+        a = x
+    elif (b-a)<e:
+        x = (b+a)/2
+
+print("Raiz =",x)
+print("Iteracoes = ", i) 
